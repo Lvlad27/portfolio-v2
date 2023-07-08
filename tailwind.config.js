2c1e532f1909e85b6afbf7c879201e36cbc1aef1
Vlad Leontescu <lvlad27@gmail.com>
2023-07-08 22:27:06 +0300

    feat: init project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    extend: {},
+    colors: {
+      "color-1": "#272727",
+      "color-2": "#202020",
+      "color-3": "#feda4e",
+      "color-4": "#a6a6a6",
+      "color-5": "#241f0b",
+      "color-6": "#f4f4f4",
+    },
+    fontFamily: {
+      sans: ["", ""],
+      serif: ["", ""],
     },
   },
   plugins: [],
-}
+};
