a8e6e92b15786f0ddf46a7ee405e5fe9a50feee2
Vlad Leontescu <lvlad27@gmail.com>
2023-07-16 20:02:53 +0300

    feat: add dev dependencies and respective configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        custom: ["BBC-Reith-Sans"],
+        custom: ['BBC-Reith-Sans'],
       },
     },
     colors: {
-      text: "#f8fafc",
-      background: "#10181e",
-      primary: "#b2c32c",
-      secondary: "#18261d",
-      accent: "#1aafea",
+      text: '#f8fafc',
+      background: '#10181e',
+      primary: '#b2c32c',
+      secondary: '#18261d',
+      accent: '#1aafea',
     },
     screens: {},
   },
